5ec1bcf976dc1504dcb998b19c9b4091106ced5b
EngineerAlexander <alexanderbijanardalan@gmail.com>
2021-08-26 22:07:07 -0700

    Update branching_and_iteration.py

--- introduction_to_computer_science_programming_in_python/branching_and_iteration.py
+++ introduction_to_computer_science_programming_in_python/branching_and_iteration.py
@@ -1,2 +1,14 @@
+# SEMANTICS TO REMEMBER ---------------------------------------------
+# input() gives string so cast if appropriate
+
 text = input("Type anything... ")
 print(5*text)
+
+# for loop structure
+Start = 0
+step = 1 # these two optional
+
+stop = 3 # end value
+
+range(start,stop,step)
+
